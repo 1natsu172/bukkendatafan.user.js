a56d317886db0b7b9161f0fb6c176ee01a2c3ff1
1natsu172 <1natsu172@users.noreply.github.com>
2017-12-19 15:08:59 +0900

    refactor fetch method

--- bukkendatafan.user.js
+++ bukkendatafan.user.js
@@ -27,8 +27,6 @@ const observeTarget = document.querySelector(".portal-entry-list");
 // トップページのエントリ記事のタイル、動的に差し込まれるのでその処理が終わるまで行儀よく待つ
 // masonryの処理でattributeが変更されるのでそれを監視する = DOMが準備された ＝ querySelectorAllができる
 const entryListObserver = new MutationObserver((mutations) => {
-  targetArticleElements = document.querySelectorAll(".entry-url");
-  articleUrls = Array.from(targetArticleElements, a => a.href);
   entryListObserver.disconnect(); //監視終了
   showBukkenData()
 });
@@ -37,58 +35,82 @@ entryListObserver.observe(observeTarget, {
   attributes: true,
 });
 
+const entryUrlArray = () => {
+  return new Promise((resolve, reject) => {
+    const targetEntryElements = document.querySelectorAll(".entry-url")
+    const entryUrls = Array.from(targetEntryElements, a => a.href)
+    resolve(entryUrls)
+  })
+}
 
-let targetArticleElements = []
-let articleUrls = []
-let resultDataArray = []
-
-const fetchArticles = urls => {
-  const asyncFetch = urls.map(async (url,index) => {
-    await fetch(url)
+const fetchEntryArray = urls => {
+  const asyncFetch = urls.map( url => {
+    return fetch(url)
       .then(response => {
         try {
           if (response.ok) {
           console.log("Fetch: ", `${response.url} => ${response.status}`)
-          return response.text(); // レスポンスをテキストとして変換する
+          return response.text() // レスポンスをテキストとして変換する
           } else {
             throw new Error(`FetchError: ${response.url} => ${response.status}`)
           }
         } catch (error) {
-          console.log(error)
-          reject(error)
+          console.error(error)
         }
       })
-      // DOM parsing
-      .then(text => new DOMParser().parseFromString(text, "text/html"))
-      // 物件の記事のDOMを整形する…… :innocent:
-      .then(document => {
-        const extractEntryData = document.querySelector(".entry-data")// 記事下部の物件データ
-        const getTableRow = extractEntryData.querySelectorAll("tr")// 物件データのtr抽出
-        const disclaimerNote = extractEntryData.querySelector("p.note")// 物件データについてる免責文章
-
-        const deleteUnnecessaryDOM = ()=> {
-          // p.note消す
-          extractEntryData.removeChild(disclaimerNote);
-          // tr要素が5つ以上あるとき配列4番目以降のtrを消す(goodroomのロゴ消したい…)
-          if (getTableRow.length >= 5) {
-            for (let index = 4; index < getTableRow.length; index++) {
-              // console.log(getTableRow)
-              getTableRow[index].parentNode.removeChild(getTableRow[index]);
-            }
-          }
-        }
-        deleteUnnecessaryDOM()
-
-        return extractEntryData;
-      })
-      // 配列に一応整形済みのDOM入れておく
-      .then(value => resultDataArray[index] = value)
-      // 各該当記事にそれぞれの物件データを挿入する
-      .then(insertElement => targetArticleElements[index].nextElementSibling.insertAdjacentElement("afterend", insertElement)
-      )
-  })
+    })
   return Promise.all(asyncFetch)
 }
+
+const extractDatafromEntry = entries => {
+  const extractedData = entries.map( entry => {
+    // DOM parse
+    const parseEntryDOM = new Promise((resolve, reject) => {
+      const parse = new DOMParser().parseFromString(entry, "text/html")
+      resolve(parse)
+    })
+    // 物件の記事のDOMを整形する…… :innocent:
+    return parseEntryDOM.then(document => {
+      const extractEntryData = document.querySelector(".entry-data")// 記事下部の物件データ
+      const getTableRow = extractEntryData.querySelectorAll("tr")// 物件データのtr抽出
+      const disclaimerNote = extractEntryData.querySelector("p.note")// 物件データについてる免責文章
+      
+      const deleteUnnecessaryDOM = ()=> {
+        // p.note消す
+        extractEntryData.removeChild(disclaimerNote);
+        // tr要素が5つ以上あるとき配列4番目以降のtrを消す(goodroomのロゴ消したい…)
+        if (getTableRow.length >= 5) {
+          for (let index = 4; index < getTableRow.length; index++) {
+            // console.log(getTableRow)
+            getTableRow[index].parentNode.removeChild(getTableRow[index]);
+          }
+        }
+      }
+      deleteUnnecessaryDOM()
+      
+      return extractEntryData;
+    })
+  })
+  return Promise.all(extractedData)
+}
+
+const insertDataIntoEntryCards = (data) => {
+  const bukkenData = data
+  console.log('bukkenData',bukkenData)
+  
+  const cards = document.querySelectorAll(".entry-url")
+  // 各エントリのカードに該当の物件データを挿入する
+  cards.forEach((card, index) => {
+    card.nextElementSibling.insertAdjacentElement("afterend", bukkenData[index])})
+}
+
+async function fetchData() {
+  const urls = await entryUrlArray().catch(error => console.error(error))
+  const fetchEntries = await fetchEntryArray(urls).catch(error => console.error(error))
+  const extractData = await extractDatafromEntry(fetchEntries).catch(error => console.error(error))
+  const insertingData = await insertDataIntoEntryCards(extractData).catch(error => console.error(error))
+}
+
 
 const stylingBukkenData = () => {
   return new Promise((resolve, reject) => {
@@ -133,6 +155,7 @@ const stylingBukkenData = () => {
   })
 }
 
+
 const reMasonry = () => {
   return new Promise((resolve, reject) => {
       if (typeof Masonry !== 'undefined') {
@@ -149,8 +172,8 @@ const reMasonry = () => {
 }
 
 async function showBukkenData() {
+  await fetchData().catch(error => console.error(error))
   await stylingBukkenData().catch(error => console.error(error))
-  await fetchArticles(articleUrls).catch(error => console.error(error))
   await reMasonry().catch(error => console.error(error))
 }
 
