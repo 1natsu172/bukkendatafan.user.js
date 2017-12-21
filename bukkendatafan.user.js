3d08ef4491a746ce446b7475a36fd10f8ef91980
1natsu172 <1natsu172@users.noreply.github.com>
2017-12-22 01:04:58 +0900

    improve MutationObserver & async process

--- bukkendatafan.user.js
+++ bukkendatafan.user.js
@@ -23,25 +23,56 @@ var inline_src = (<><![CDATA[
     // Your code here...
     console.log('[BukkenDataFan]','Hi, BukkenDataFan\'s bro!')
 
-// 監視対象のノードを取得
-const observeTarget = document.querySelector(".portal-entry-list");
+function PromiseMutationObserver(element, attributeName, rejectTime = 0) {
+    return new Promise((resolve,reject) => {
+        let hasChanged = false
+        const observerConfig = {attributes: true, attributeOldValue:true}
+        const observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+              if (attributeName && mutation.attributeName == attributeName) {
+                  hasChanged = true
+                  observer.disconnect()
+                  resolve(element, element.getAttribute(attributeName))
+              }
+              
+              if (!attributeName) {
+                  hasChanged = true
+                  observer.disconnect()
+                  resolve(element)
+              }
+          })
+        })
+        
+        if (rejectTime > 0) {
+            window.setTimeout(()=>{
+                if (!hasChanged) {
+                    reject(element)
+                }
+            },rejectTime * 100)
+        }
+        
+        if (attributeName) observerConfig.attributeFilter = [attributeName]
+        observer.observe(element, observerConfig)
+    })
+}
 
-// トップページのエントリ記事のタイル、動的に差し込まれるのでその処理が終わるまで行儀よく待つ
-// masonryの処理でattributeが変更されるのでそれを監視する = DOMが準備された ＝ querySelectorAllができる
-const entryListObserver = new MutationObserver((mutations) => {
-  entryListObserver.disconnect(); //監視終了
-  showBukkenData()
-});
-// 監視メソッドを設定
-entryListObserver.observe(observeTarget, {
-  attributes: true,
-});
+const observeEntryList = () => {
+  // **
+  // 物件データの差し込み先が出来上がるのを監視して、データ差し込みアクションを待機する
+  // Masonryが走る = attributeが変更される = エントリーリストが出来上がった = 物件データ差し込めるの意
+  // *
+
+  // 監視対象のnode
+  const target = document.querySelector(".portal-entry-list")
+  // 監視開始
+  return PromiseMutationObserver(target)
+}
 
 const setReadMoreButtonEvent = json => {
   const readMoreRef = json[0].next_ref
-  // console.log('ref',readMoreRef)
   const readMoreButton = document.querySelector('.portal-entry-load-more')
-  readMoreButton.addEventListener('click', function(){showBukkenData(readMoreRef)}, {
+  readMoreButton.addEventListener('click', function() {showBukkenData(readMoreRef)},
+  {
     capture: false,
     once: true,
     passive: false
@@ -54,10 +85,12 @@ const entriesJsonUrlReturner = ref => {
   const pageSize = tag && tagType != 0 ? 10 : 30
   const refParam = ref != null ? `&ref=${ encodeURIComponent(ref) }` : ''
   const url = `/entries.json?limit=${ pageSize }&tag=${ encodeURIComponent(tag) }${ refParam }`
+  // console.log(url)
   return [url]
 }
 
 const fetchJson = urls => {
+  console.log('[BukkenDataFan]', `Getting BukkenData…`)
   const asyncFetch = urls.map( url => {
     return fetch(url)
       .then(response => {
@@ -127,17 +160,17 @@ const createBukkenData = json => {
 
 const insertDataIntoEntryCards = (data) => {
   const bukkenData = data
-  let cards = document.querySelectorAll(".entry-url")
+  let cards = [...document.querySelectorAll(".entry-url")]
   // もっとみるボタン押下したとき、物件データの数で追加分のDOM箇所を判定する
-  if (cards.length > 30) { cards = [...cards].slice(-bukkenData.length) }
+  if (cards.length > 30) { cards = cards.slice(-bukkenData.length) }
   
   // console.log(bukkenData.length)
   // console.log(cards)
 
   // 各エントリのカードに該当の物件データを挿入する
-  cards.forEach((card, index) => {
+  return Promise.all(cards.map((card, index) => {
     card.nextElementSibling.insertAdjacentHTML("afterend", bukkenData[index])
-  })
+  }))
 }
 
 
@@ -200,22 +233,38 @@ const reMasonry = () => {
   })
 }
 
-
-async function getAndInsertBukkenData(ref) {
-  const urls = entriesJsonUrlReturner(ref)
-  const fetch = await fetchJson(urls).catch(error => console.error(error))
-  const data = await createBukkenData(fetch).catch(error => console.error(error))
-  const insertingData = insertDataIntoEntryCards(data)
-  setReadMoreButtonEvent(fetch)
+// getDataFunc
+async function getBukkenData(ref) {
+  const insertReadyFlag = observeEntryList().catch(error => console.error(error))
+  async function getDataFlow() {
+    const urls = entriesJsonUrlReturner(ref)
+    const fetch = await fetchJson(urls).catch(error => console.error(error))
+    const createDataDOM = await createBukkenData(fetch).catch(error => console.error(error))
+    const setEvent = setReadMoreButtonEvent(fetch)
+    return createDataDOM
+  }
+  // 挿入先DOMの監視と差し込むデータの準備を並列で待つ
+  return Promise.all([insertReadyFlag, getDataFlow()]).then(data => {
+    return data[1] // 整形済みDOMデータだけ返す
+  })
 }
 
+// InsertDataFunc
+async function insertBukkenData(data) {
+  insertDataIntoEntryCards(data)
+}
 
+// mainFunc
 async function showBukkenData(ref) {
-  await getAndInsertBukkenData(ref).catch(error => console.error(error))
+  const getData = await getBukkenData(ref).catch(error => console.error(error))
+  // console.log(getData)
+  const insertData = await insertBukkenData(getData).catch(error => console.error(error))
   await stylingBukkenData().catch(error => console.error(error))
   await reMasonry().catch(error => console.error(error))
   console.log('[BukkenDataFan]','Inserting BukkenData completed, Enjoy!')
 }
+showBukkenData()
+
 
 // Your code end...
 
