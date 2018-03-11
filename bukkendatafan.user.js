277ee8ece31f8f25ad763507e2b6cbc85672c10e
1natsu172 <1natsu172@users.noreply.github.com>
2018-03-11 13:30:31 +0900

    Fix for new site design

--- bukkendatafan.user.js
+++ bukkendatafan.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bukkendatafan
 // @namespace    https://github.com/1natsu172/bukkendatafan.user.js
-// @version      1.0.2
+// @version      1.1.0
 // @description  物件ファンのトップページの各記事に物件の賃料や所在地などの情報を表示するユーザースクリプト
 // @author       1natsu
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
@@ -22,128 +22,143 @@ var inline_src = (<><![CDATA[
     /* jshint asi: true */
 
     // Your code here...
-    console.log('[BukkenDataFan]','Hi, BukkenDataFan\'s bro!')
+    console.log("[BukkenDataFan", "Hi, BukkenDataFan's bro!");
 
-function PromiseMutationObserver(element, attributeName, rejectTime = 0) {
-    return new Promise((resolve,reject) => {
-        let hasChanged = false
-        const observerConfig = {attributes: true, attributeOldValue:true}
-        const observer = new MutationObserver((mutations) => {
-          mutations.forEach((mutation) => {
-              if (attributeName && mutation.attributeName == attributeName) {
-                  hasChanged = true
-                  observer.disconnect()
-                  resolve(element, element.getAttribute(attributeName))
-              }
-              
-              if (!attributeName) {
-                  hasChanged = true
-                  observer.disconnect()
-                  resolve(element)
-              }
-          })
-        })
-        
-        if (rejectTime > 0) {
-            window.setTimeout(()=>{
-                if (!hasChanged) {
-                    reject(element)
-                }
-            },rejectTime * 100)
+function waitElement(selector, options) {
+  options = Object.assign(
+    {
+      target: document
+    },
+    options
+  );
+
+  const checkElement = selector => {
+    return options.target.querySelector(selector);
+  };
+
+  return new Promise((resolve, reject) => {
+    let observer = {};
+    let _hasObserved = false;
+
+    const configs = {
+      childList: true,
+      subtree: true
+    };
+
+    // Checking already element existed.
+    const element = checkElement(selector);
+    if (element) {
+      _hasObserved = true;
+      return resolve(element);
+    }
+
+    observer = new MutationObserver(mutations => {
+      mutations.some(() => {
+        const element = checkElement(selector);
+
+        if (element) {
+          observer.disconnect();
+          _hasObserved = true;
+          resolve(element);
+          return true; // The same as "break" in `Array.some()`
         }
-        
-        if (attributeName) observerConfig.attributeFilter = [attributeName]
-        observer.observe(element, observerConfig)
-    })
-}
 
-const observeEntryList = () => {
-  // **
-  // 物件データの差し込み先が出来上がるのを監視して、データ差し込みアクションを待機する
-  // Masonryが走る = attributeが変更される = エントリーリストが出来上がった = 物件データ差し込めるの意
-  // *
+        return false;
+      });
+    });
 
-  // 監視対象のnode
-  const target = document.querySelector(".portal-entry-list")
-  // 監視開始
-  return PromiseMutationObserver(target)
+    // Start observe.
+    observer.observe(options.target, configs);
+  });
 }
 
 const setReadMoreButtonEvent = json => {
-  const readMoreRef = json[0].next_ref
-  const readMoreButton = document.querySelector('.portal-entry-load-more')
-  readMoreButton.addEventListener('click', function() {showBukkenData(readMoreRef)},
-  {
-    capture: false,
-    once: true,
-    passive: false
-  })
-}
+  const readMoreRef = json[0].next_ref;
+  const readMoreButton = document.querySelector(".portal-entry-load-more");
+  readMoreButton.addEventListener(
+    "click",
+    function() {
+      showBukkenData(readMoreRef);
+    },
+    {
+      capture: false,
+      once: true,
+      passive: false
+    }
+  );
+};
 
 const entriesJsonUrlReturner = ref => {
-  const tag = document.documentElement.getAttribute ('data-tag') || ''
-  const tagType = document.documentElement.getAttribute ('data-tag-type') || 0
-  const pageSize = tag && tagType != 0 ? 10 : 30
-  const refParam = ref != null ? `&ref=${ encodeURIComponent(ref) }` : ''
-  const url = `/entries.json?limit=${ pageSize }&tag=${ encodeURIComponent(tag) }${ refParam }`
+  const tag = document.documentElement.getAttribute("data-tag") || "";
+  const tagType = document.documentElement.getAttribute("data-tag-type") || 0;
+  const pageSize = tag && tagType != 0 ? 10 : 12;
+  const refParam = ref != null ? `&ref=${encodeURIComponent(ref)}` : "";
+  const url = `/entries.json?limit=${pageSize}&tag=${encodeURIComponent(
+    tag
+  )}${refParam}`;
   // console.log(url)
-  return [url]
-}
+  return [url];
+};
 
 const fetchJson = urls => {
-  console.log('[BukkenDataFan]', `Getting BukkenData…`)
-  const asyncFetch = urls.map( url => {
-    return fetch(url)
-      .then(response => {
-        try {
-          if (response.ok) {
-          console.log('[BukkenDataFan]', `Fetch: ${response.url} => response: ${response.status}`)
-          return response.json() // レスポンスをjsonとして処理する
-          } else {
-            throw new Error(`[BukkenDataFan] FetchError: ${response.url} => response: ${response.status}`)
-          }
-        } catch (error) {
-          console.error(error)
+  console.log("[BukkenDataFan]", `Getting BukkenData…`);
+  const asyncFetch = urls.map(url => {
+    return fetch(url).then(response => {
+      try {
+        if (response.ok) {
+          console.log(
+            "[BukkenDataFan]",
+            `Fetch: ${response.url} => response: ${response.status}`
+          );
+          return response.json(); // レスポンスをjsonとして処理する
+        } else {
+          throw new Error(
+            `[BukkenDataFan] FetchError: ${response.url} => response: ${
+              response.status
+            }`
+          );
         }
-      })
-    })
-  return Promise.all(asyncFetch)
-}
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  });
+  return Promise.all(asyncFetch);
+};
 
 const createBukkenData = json => {
-  const entriesArray = json[0].entries
-  const createDOM = entriesArray.map( entry => {
+  const entriesArray = json[0].entries;
+  const createDOM = entriesArray.map(entry => {
     return new Promise((resolve, reject) => {
       const dataObject = {
         所在地: entry.data.addr,
         賃料: entry.data.chinryou,
         売価: entry.data.baika,
         面積: entry.data.menseki,
-        最寄り: entry.data.moyori,
-      }
+        最寄り: entry.data.moyori
+      };
       // dataの中からvalueがあるものだけ抽出する
       const filteredData = Object.keys(dataObject)
-                                 .filter(key => dataObject[key] !== '') // 条件で絞り込む
-                                 .reduce((obj, key) => {
-                                   obj[key] = dataObject[key]
-                                   return obj
-                                 }, {})
+        .filter(key => dataObject[key] !== "") // 条件で絞り込む
+        .reduce((obj, key) => {
+          obj[key] = dataObject[key];
+          return obj;
+        }, {});
 
       function loopTableData() {
-        let tableData = ''
-        for(let key of Object.keys(filteredData)) {
+        let tableData = "";
+        for (let key of Object.keys(filteredData)) {
           tableData += `
             <tr>
               <th>${key}</th>
               <td>${filteredData[key]}</td>
             </tr>
-          `
+          `;
         }
-        return tableData
+        return tableData;
       }
 
-      const dataDOMLiteral =
-        `
+      const dataDOMLiteral = `
         <div class="entry-data">
           <table>
             <tbody>
@@ -151,39 +166,51 @@ const createBukkenData = json => {
             </tbody>
           </table>
         </div>
-        `
+        `;
 
-      resolve(dataDOMLiteral)
-    })
-  })
-  return Promise.all(createDOM)
-}
+      resolve(dataDOMLiteral);
+    });
+  });
+  return Promise.all(createDOM);
+};
 
-const insertDataIntoEntryCards = (data) => {
-  const bukkenData = data
-  let cards = [...document.querySelectorAll(".entry-url")]
+const insertDataIntoEntryCards = data => {
+  const bukkenData = data;
+  const newerEntryFeed = document.querySelector(".portal-entries");
+  let cards = [...newerEntryFeed.querySelectorAll(".entry-url")];
   // もっとみるボタン押下したとき、物件データの数で追加分のDOM箇所を判定する
-  if (cards.length > 30) { cards = cards.slice(-bukkenData.length) }
-  
+  if (cards.length > 12) {
+    cards = cards.slice(-bukkenData.length);
+  }
+
   // console.log(bukkenData.length)
   // console.log(cards)
 
   // 各エントリのカードに該当の物件データを挿入する
-  return Promise.all(cards.map((card, index) => {
-    card.nextElementSibling.insertAdjacentHTML("afterend", bukkenData[index])
-  }))
-}
-
+  return Promise.all(
+    cards.map((card, index) => {
+      card.nextElementSibling.insertAdjacentHTML("afterend", bukkenData[index]);
+    })
+  );
+};
 
 const stylingBukkenData = () => {
   return new Promise((resolve, reject) => {
     const customCSSContent = `
       <style>
+        .portal-entry-list {
+          display: flex;
+          flex-wrap: wrap;
+        }
+        .portal-entry {
+          height: auto;
+        }
+
         .entry-data {
-            padding: 15px 10px;
-            color: #999;
-            /* opacity: 0.1; */
-            transition: opacity 0.3s;
+          padding: 15px 0px;
+          color: #999;
+          /* opacity: 0.1; */
+          transition: opacity 0.3s;
         }
         .card:hover .entry-data {
           opacity: 1;
@@ -193,17 +220,17 @@ const stylingBukkenData = () => {
           width: 100%;
         }
         .entry-data tr {
-            border-bottom: solid 1px #e6e6e6;
+          border-bottom: solid 1px #e6e6e6;
         }
         .entry-data th {
-            width: 20%;
-            font-weight: normal;
-            font-size: 12px;
-            white-space: nowrap;
+          width: 20%;
+          font-weight: normal;
+          font-size: 12px;
+          white-space: nowrap;
         }
         .entry-data td {
-            padding: 8px 0px 8px 10px;
-            font-size: 13px;
+          padding: 8px 0px 8px 10px;
+          font-size: 13px;
         }
       </style>
     `;
@@ -211,79 +238,65 @@ const stylingBukkenData = () => {
     // adding <style>~~~</style>  to <head>
     if (document.head) {
       document.head.insertAdjacentHTML("beforeend", customCSSContent);
-      resolve()
+      resolve();
     } else {
-      reject('headタグ見当たらない')
+      reject("headタグ見当たらない");
     }
-  })
-}
-
-
-const reMasonry = () => {
-  return new Promise((resolve, reject) => {
-      if (typeof Masonry !== 'undefined') {
-        setTimeout(() => {
-          var msnry = new Masonry(".portal-entry-list", {
-            itemSelector: ".portal-entry", // target item
-            columnWidth: 240,
-            gutter: 15 // margin
-          })
-          resolve()
-        }, 500)
-      } else {
-        reject('Masonry見当たらない')
-      }
-  })
-}
-
+  });
+};
 
 const waitImagesloaded = () => {
-  return new Promise((resolve,reject)=> {
-    const elem = document.querySelectorAll('.entry-image')
-    const observeImages = new imagesLoaded(elem, {background: true})
-    observeImages.on('always', instance => {
-      resolve()
-    })
-    .on('progress', (instance, image) => {
-    })
-  })
-}
-
+  return new Promise((resolve, reject) => {
+    const elem = document.querySelectorAll(".entry-image");
+    const observeImages = new imagesLoaded(elem, { background: true });
+    observeImages
+      .on("always", instance => {
+        resolve();
+      })
+      .on("progress", (instance, image) => {});
+  });
+};
 
 // getDataFunc
 async function getBukkenData(ref) {
-  const insertReadyFlag = observeEntryList().catch(error => console.error(error))
+  const insertReadyFlag = waitElement(".portal-entry-list").catch(error =>
+    console.error(error)
+  );
   async function getDataFlow() {
-    const urls = entriesJsonUrlReturner(ref)
-    const fetch = await fetchJson(urls).catch(error => console.error(error))
-    const createDataDOM = await createBukkenData(fetch).catch(error => console.error(error))
-    const setEvent = setReadMoreButtonEvent(fetch)
-    return createDataDOM
+    const urls = entriesJsonUrlReturner(ref);
+    const fetch = await fetchJson(urls).catch(error => console.error(error));
+    const createDataDOM = await createBukkenData(fetch).catch(error =>
+      console.error(error)
+    );
+    const setEvent = setReadMoreButtonEvent(fetch);
+    return createDataDOM;
   }
   // 挿入先DOMの監視と差し込むデータの準備を並列で待つ
   return Promise.all([insertReadyFlag, getDataFlow()]).then(data => {
-    return data[1] // 整形済みDOMデータだけ返す
-  })
+    return data[1]; // 整形済みDOMデータだけ返す
+  });
 }
 
 // InsertDataFunc
 async function insertBukkenData(data) {
-  await insertDataIntoEntryCards(data)
+  await insertDataIntoEntryCards(data);
 }
 
 // mainFunc
 async function showBukkenData(ref) {
-  const getData = await getBukkenData(ref).catch(error => console.error(error))
-  // console.log(getData)
-  const insertData = await insertBukkenData(getData).then(()=> 
-  console.log('[BukkenDataFan]','Inserted BukkenData')).catch(error => console.error(error))
-  await stylingBukkenData().catch(error => console.error(error)).then(()=> console.log('[BukkenDataFan]','Injected StyleSheet'))
-  await waitImagesloaded().then(()=> console.log('[BukkenDataFan]','Loaded cards image'))
-  await reMasonry().then(()=> console.log('[BukkenDataFan]','Relocated cards')).catch(error => console.error(error))
-  console.log('[BukkenDataFan]','BukkenData completed, Enjoy!')
+  const getData = await getBukkenData(ref).catch(error => console.error(error));
+  const insertData = await insertBukkenData(getData)
+    .then(() => console.log("[BukkenDataFan]", "Inserted BukkenData"))
+    .catch(error => console.error(error));
+  await stylingBukkenData()
+    .catch(error => console.error(error))
+    .then(() => console.log("[BukkenDataFan]", "Injected StyleSheet"));
+  await waitImagesloaded().then(() =>
+    console.log("[BukkenDataFan]", "Loaded cards image")
+  );
+  console.log("[BukkenDataFan]", "BukkenData completed, Enjoy!");
 }
-showBukkenData()
-
+showBukkenData();
 
 // Your code end...
 
